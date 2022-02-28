1f08ec4081c4469df64fb029d30ab0e8bf12f97f
Ahtasham619c <Ahtashamahmad25@gmail.com>
2022-02-28 20:20:52 +0500

    tasks displayed

--- pages/board/index.tsx
+++ pages/board/index.tsx
@@ -1,11 +1,24 @@
 import { useQuery } from '@apollo/client';
+import { Row } from 'react-bootstrap';
 import tasksQuery from './tasks';
 
 const Board = () => {
   const { data, loading } = useQuery(tasksQuery, {
     onCompleted: (data) => console.log(data),
   });
-  return <div>Board</div>;
+  return (
+    <div className="pt-3 h-100 d-flex flex-column">
+      <Row>
+        <h1>Project title</h1>
+      </Row>
+      <div className="board-container d-flex flex-row flex-grow-1">
+        {data &&
+          data.tasks.map((task: any) => {
+            return <div key={task.id}>{task.title}</div>;
+          })}
+      </div>
+    </div>
+  );
 };
 
 export default Board;
